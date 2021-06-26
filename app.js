6a14a70717fb183ad0e96c2ef5af66f739290d2f
codesleepcode <81725710+codesleepcode@users.noreply.github.com>
2021-06-26 15:46:48 +1000

    Update app.js

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const app = express();
 const port = 3000;
 
 app.get('/',  (req,res) => {
-  res.send("<html><h1>welcome to my app</h1> <h2> version 1.0 </h2> </html>")
+  res.send("<html><h1>welcome to my app</h1> <h2> version 2.0 </h2> </html>")
 })
 
 app.get('/products', (req,res) => {
